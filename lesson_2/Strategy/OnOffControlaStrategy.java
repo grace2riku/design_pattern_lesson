640e475e566459425dbc15f2d28b215c4f217984
Koji Abe <gracehime@gmail.com>
2024-11-10 23:40:08 +0900

    サンプルプログラムのコードを書いた

---
+++ lesson_2/Strategy/OnOffControlaStrategy.java
@@ -0,0 +1,24 @@
+
+public class OnOffControlaStrategy implements ControlStrategy {
+
+    public OnOffControlaStrategy() {
+    }
+
+    @Override
+    public int CalcOperationAmount(int targetValue, int measuredValue, int time) {
+        String className = new Object(){}.getClass().getEnclosingClass().getName();
+        String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        System.out.println(className + " " + methodName);
+
+        return 0;
+    }
+
+    @Override
+    public void init() {
+        String className = new Object(){}.getClass().getEnclosingClass().getName();
+        String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        System.out.println(className + " " + methodName);
+
+        return;
+    }    
+}
